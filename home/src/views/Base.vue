a13f036fceef8ce385a8c9301b3e65c214da831e
humorliang <humorliang@163.com>
2019-05-09 13:17:11 +0800

    feat(footer): 底部信息

--- home/src/views/Base.vue
+++ home/src/views/Base.vue
@@ -17,7 +17,11 @@
             </Col>
         </Row>
     </div>
-    <div id="footer">底部</div>
+    <div id="footer" style="text-align:center">
+        <p> 版权所有：© 201７-2019 liangcode.cn</p>
+        <p> 本站前端采用vue + ivew框架,后端GO + Gin，托管在阿里云</p>
+        <p> 联系邮箱：humorliang@163.com</p>
+    </div>
 </div>
 </template>
 
@@ -49,39 +53,35 @@ export default {
         RightSider,
         Nav
     },
-    created(){
-        let _this=this
+    created() {
+        let _this = this
         //获取分类
         this.axios.get("/api/v1/admin/auth/terms").then(function (res) {
             // console.log(res);
-            if (res.data.code==0) {
-                _this.rightSliderData.terms.list=res.data.data
-            } else {
-            }
+            if (res.data.code == 0) {
+                _this.rightSliderData.terms.list = res.data.data
+            } else {}
         })
         //获取趣事
         this.axios.get("/api/v1/home/fun").then(function (res) {
             // console.log(res);
-            if (res.data.code==0) {
-                _this.rightSliderData.notes.content=res.data.data.content
-            } else {
-            }
+            if (res.data.code == 0) {
+                _this.rightSliderData.notes.content = res.data.data.content
+            } else {}
         })
         //联系
-         this.axios.get("/api/v1/home/concat").then(function (res) {
+        this.axios.get("/api/v1/home/concat").then(function (res) {
             // console.log(res);
-            if (res.data.code==0) {
-                _this.rightSliderData.concat.content=res.data.data.content
-            } else {
-            }
+            if (res.data.code == 0) {
+                _this.rightSliderData.concat.content = res.data.data.content
+            } else {}
         })
         //菜单导航
-         this.axios.get("/api/v1/home/navs").then(function (res) {
+        this.axios.get("/api/v1/home/navs").then(function (res) {
             console.log(res);
-            if (res.data.code==0) {
-                _this.navData=res.data.data
-            } else {
-            }
+            if (res.data.code == 0) {
+                _this.navData = res.data.data
+            } else {}
         })
     }
 };
